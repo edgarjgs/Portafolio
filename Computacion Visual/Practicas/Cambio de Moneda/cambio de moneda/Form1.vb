d21cc1e978bf509e3e86270bcf58fd1149efe245
edgarjgs <97697559+edgarjgs@users.noreply.github.com>
2022-11-22 18:35:52 -0400

    Add files via upload
    visual basic

---
+++ Computacion Visual/Practicas/Cambio de Moneda/cambio de moneda/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Text = Val(TextBox1.Text) * 0.90205 'euro
+        TextBox3.Text = Val(TextBox1.Text) * 0.76372 'british  
+        TextBox4.Text = Val(TextBox1.Text) * 109.93 'japan
+    End Sub
+End Class
